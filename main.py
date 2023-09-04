c44b303271503f516cb0136a6d6c66907a97e9bf
Paul Schnisa <paul.schnisa@proton.me>
2023-09-04 15:34:45 +0200

    update main.py

--- main.py
+++ main.py
@@ -10,35 +10,66 @@ player_speed = 5
 map = [
     [1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 1, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 0, 1, 1, 1],
+    [1, 0, 1, 0, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1]
 ]
+
+# Flags to track key states
+move_forward = False
+move_backward = False
+turn_left = False
+turn_right = False
 
 window = pyglet.window.Window(800, 600, "PRayCast")
 
 @window.event
 def on_key_press(symbol, modifiers):
-    global pa, px, py
+    global pa, px, py, move_forward, move_backward, turn_left, turn_right
     if symbol == key.W:
+        move_forward = True
+    elif symbol == key.A:
+        turn_left = True
+    elif symbol == key.S:
+        move_backward = True
+    elif symbol == key.D:
+        turn_right = True
+
+@window.event
+def on_key_release(symbol, modifiers):
+    global move_forward, move_backward, turn_left, turn_right
+    if symbol == key.W:
+        move_forward = False
+    elif symbol == key.A:
+        turn_left = False
+    elif symbol == key.S:
+        move_backward = False
+    elif symbol == key.D:
+        turn_right = False
+
+def update_player_position(dt):
+    global pa, px, py
+    if move_forward:
         new_px = px + math.sin(pa*math.pi/180) * player_speed
         new_py = py + math.cos(pa*math.pi/180) * player_speed
         if not is_collision(new_px, new_py):
             px, py = new_px, new_py
-    elif symbol == key.A:
-        pa -= 10
-    elif symbol == key.S:
+    if move_backward:
         new_px = px - math.sin(pa*math.pi/180) * player_speed
         new_py = py - math.cos(pa*math.pi/180) * player_speed
         if not is_collision(new_px, new_py):
             px, py = new_px, new_py
-    elif symbol == key.D:
+    if turn_left:
+        pa -= 10
+    if turn_right:
         pa += 10
 
-def drawPlayer(px, py, a):
+pyglet.clock.schedule_interval(update_player_position, 1/60.0)
+
+def draw_player(px, py, a):
     plx = px + math.sin(a*math.pi/180)*10
     ply = py + math.cos(a*math.pi/180)*10
     player = pyglet.shapes.Line(px, py, plx, ply, width=2, color=(255, 0, 0))
@@ -63,7 +94,7 @@ class Box:
         box = pyglet.shapes.BorderedRectangle(self.x, self.y, self.width, self.height, border_color=self.borderColor, color=self.color)
         box.draw()
 
-def drawMap():
+def draw_map():
     for i, row in enumerate(map):
         for j, cell in enumerate(row):
             if cell == 1:
@@ -86,7 +117,7 @@ def is_collision(new_px, new_py):
 @window.event
 def on_draw():
     window.clear()
-    drawMap()
-    drawPlayer(px, py, pa)
+    draw_map()
+    draw_player(px, py, pa)
 
 pyglet.app.run()
